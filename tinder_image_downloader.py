4611cc4028de428f8c10d75fc04aa47886393863
gradealx <alexandegr@edu.aau.at>
2022-04-22 14:06:18 +0200

    implemented image downloader

---
+++ tinder_image_downloader.py
@@ -0,0 +1,23 @@
+import pynder
+import urllib.request
+
+
+def main():
+    XAuthToken = 'your token'
+    session = pynder.Session(XAuthToken=XAuthToken)
+    session.update_location(55.671865, 12.483533)
+
+    users = session.nearby_users()
+    for user in users:
+        photos: []
+        photos = user.get_photos()
+        cnt = 0
+        for photo in photos:
+            image_name = "downloads/" + user.name + "_" + str(cnt) + "_" + str(user.age) + "_" + user.id + ".jpg"
+            urllib.request.urlretrieve(photo, image_name)
+            cnt += 1
+            print(image_name)
+
+
+if __name__ == "__main__":
+    main()
